d5bfbce110a1310c61cf2d8f6344bcba8af49cd1
fjrodriguez <fjrodriguez@ufl.edu>
2021-04-20 16:16:00 -0400

    Added test folder with index.html to test subdomains

---
+++ public/test/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<?php include './common/head.html'; ?>
+	<meta name="description" content="This is a test.">
+	<style>
+	img {
+    	display: block;
+    	margin-left: auto;
+    	margin-right: auto;
+    	height: auto;
+    	width: 75%;
+	}
+	</style>
+</head>
+<body>
+<?php include './common/nav.html'; ?>
+<div class="front_page">
+<br>
+<!--img id="learndialogue_group_photo" src="img/learndialogue_fall2020.png" class="center" /-->
+
+<p>This is a test</p>
+
+
+</div>
+<?php include './common/footer.html'; ?>
+</body>
+</html>
+
+
