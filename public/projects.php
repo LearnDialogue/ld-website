03039b39c48618835187dc4b5aaef41ed396d99f
Timothy D. Brown <42791345+tdbrown76@users.noreply.github.com>
2025-06-18 16:53:44 -0400

    Updated AI Empowered Athletics to AI Powered Athletics

--- public/projects.php
+++ public/projects.php
@@ -55,7 +55,7 @@
 				<a href="project.php?id=AI-EmpoweredAthlete">
 				
 				<img src="projects/AI-EmpoweredAthlete.png" alt="AI-EmpoweredAthletics">
-				<span class"thumb_txt"><h3>AI Empowered Athletics</h3>
+				<span class"thumb_txt"><h3>AI Powered Athletics</h3>
 					Supporting student-athlete wellness and performance using data and AI
 			</a>
 			
