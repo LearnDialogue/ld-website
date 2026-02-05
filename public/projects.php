6ade6af74023c28b620acfb3899a291c31c0f5eb
Timothy D. Brown <42791345+tdbrown76@users.noreply.github.com>
2026-02-05 10:50:15 -0500

    Adding hyphen to AI-Powered Athletics

--- public/projects.php
+++ public/projects.php
@@ -44,7 +44,7 @@
 				<a href="project.php?id=AI-EmpoweredAthlete">
 				
 				<img src="projects/AI-EmpoweredAthlete.png" alt="AI-EmpoweredAthletics">
-				<span class"thumb_txt"><h3>AI Powered Athletics</h3>
+				<span class"thumb_txt"><h3>AI-Powered Athletics</h3>
 					Supporting student-athlete wellness and performance using data and AI
 			</a>
 			
