836781399d1237e32fb7384d2358f1671b4953ec
John Terracina <john@learndialogue.org>
2026-04-03 13:36:19 -0400

    fix siteimprove issues on homepage

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<?php include './common/head.html'; ?>
 	<meta name="description" content="The LearnDialogue Group, led by Kristy Boyer, is dedicated to dramatically improving human teaching and learning with adaptive technologies. We develop systems that support individual and collaborative learning, including tutorial dialogue systems, intelligent tutoring systems, and game-based learning environments.">
@@ -20,7 +20,7 @@
 <?php include './common/nav.html'; ?>
 <div class="front_page">
 <br>
-<img id="learndialogue_group_photo" src="img/lg-group-photo.jpg" class="center" />
+<img id="learndialogue_group_photo" src="img/lg-group-photo.jpg" class="center" alt="LearnDialogue Group Photo"/>
 
 <p>The LearnDialogue Group is a research lab in the <a href="https://www.cise.ufl.edu/">Department of Computer &amp; Information Science &amp; Engineering</a> at the <a href="http://www.ufl.edu/">University of Florida</a>. We are a group of computer scientists with multidisciplinary backgrounds, led by <a href="person.php?id=keboyer">Kristy Elizabeth Boyer</a>.</p>
 
